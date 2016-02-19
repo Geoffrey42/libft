ed30e1e9e63d5df9b75a7d162e46fd8699ccb1a1
Geoffrey GANE <ggane@e1r3p4.42.fr>
2016-02-19 21:18:02 +0100

    ajout ft_memcmp + test correspondant (warning incomprehensible

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/19 19:46:42 by ggane             #+#    #+#             */
+/*   Updated: 2016/02/19 21:10:52 by ggane            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*p1;
+	unsigned char	*p2;
+
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	while (n > 0)
+	{
+		if (*p1++ - *p2++ > 0)
+			return (*p1++ - *p2++);
+		else if (*p1++ - *p2++ < 0)
+			return (*p1++ - *p2++);
+		n--;
+	}
+	return (0);
+}
